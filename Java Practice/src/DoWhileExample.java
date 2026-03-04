542e032f5194fa29cfec567b157a90bda72205fb
PranithaGandham <gpranitha2003@gmail.com>
2026-03-04 14:52:56 +0530

    java lesson 5

---
+++ Java Practice/src/DoWhileExample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DoWhileExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+
+        do{
+            System.out.print("Enter a positive number: ");
+            number = sc.nextInt();
+        }
+        while(number <= 0);
+
+        System.out.println("You entered: " + number);
+
+        sc.close();
+    }
+}
